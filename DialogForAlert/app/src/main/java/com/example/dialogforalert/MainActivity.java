45da99945cd44b7a35ebedef0b9237fa475eca5b
wael <waelxvv@gmail.com>
2020-12-13 13:25:01 +0100

    TP3.1

---
+++ DialogForAlert/app/src/main/java/com/example/dialogforalert/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.dialogforalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new
+                AlertDialog.Builder(MainActivity.this);
+        myAlertBuilder.setTitle(R.string.alert_title);
+        myAlertBuilder.setMessage(R.string.alert_message);
+
+        myAlertBuilder.setPositiveButton(R.string.ok_button, new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), "Pressed OK",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.setNegativeButton(R.string.cancel_button, new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), "Pressed Cancel",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.show();
+    }
+
+}
